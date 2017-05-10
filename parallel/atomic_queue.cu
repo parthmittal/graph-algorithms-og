087ead83de209fa4f8597867795311931a79a05f
parthmittal <parth15069@iiitd.ac.in>
2017-05-10 18:29:38 +0530

    atomic_queue: compiles now

--- parallel/atomic_queue.cu
+++ parallel/atomic_queue.cu
@@ -1,3 +1,5 @@
+#include "atomic_queue.cuh"
+
 __device__ void atomic_push(queue_t Q, int element)
 {
 	int idx = atomicAdd(Q.head, 1);
--- parallel/atomic_queue.cuh
+++ parallel/atomic_queue.cuh
@@ -1,6 +1,11 @@
+#ifndef __ATOMIC_QUEUE_cuh
+#define __ATOMIC_QUEUE_cuh
+
 typedef struct __queue_t {
 	int *data;
 	int *head;
 } queue_t;
 
 __device__ void atomic_push(int element);
+
+#endif // __ATOMIC_QUEUE_cuh
