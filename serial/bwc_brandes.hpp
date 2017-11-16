f1dc96526cf6e8da115a1084698e09e20fff6721
parthmittal <parth15069@iiitd.ac.in>
2017-11-16 14:33:26 +0530

    made brandes1() a function and available outside file too

--- serial/bwc_brandes.hpp
+++ serial/bwc_brandes.hpp
@@ -3,11 +3,10 @@
 #ifndef __BWC_BRANDES_hpp__
 #define __BWC_BRANDES_hpp__
 
-#ifdef __BRANDES__
+void
+brandes1(const undirected_graph_t &G, int source, std::vector<double> &BC);
 
 std::vector<double>
 compute_bwc(const undirected_graph_t &G);
-
-#endif // __BRANDES__
 
 #endif // __BWC_BRANDES_hpp__
