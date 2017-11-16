077038c2de2d2fabd90b47a57a9df922d04e8186
parthmittal <parth15069@iiitd.ac.in>
2017-11-16 19:57:31 +0530

    cleaned up main()

--- serial/test.cpp
+++ serial/test.cpp
@@ -47,6 +47,8 @@ int main()
 
 	#else
 		#ifdef __USE_DFS_TREE__
+		auto start = chrono::steady_clock::now();
+
 		bwc_our bwc(g);
 		bwc.sim_brandes_all();
 
@@ -54,54 +56,10 @@ int main()
 			printf("%F\n", bwc.bwc[i]);
 		}
 
-		auto start = chrono::steady_clock::now();
-		two_connected_prop T(g);
-		T.ear_decompose();
-
-		/*
-		 * at this point, we are done with computing the ear-decomposition,
-		 * we will just spend the rest of the time pretty printing
-		 */
-
 		auto finish = chrono::steady_clock::now();
 		auto int_ms = chrono::duration_cast<chrono::milliseconds>(finish - start);
 
 		cerr << "computed bwc in " << int_ms.count() << " milliseconds" << endl;
-
-		auto vec = T.ear_decomposition;
-		/* code used to print output in format for asc24b
-		 * problem B over here(http://codeforces.com/gym/100340)
-		 * this is useful to test any changes we make to algorithm,
-		 * so I'm leaving it on here.
-		 */
-		//	if (vec.size() > 2) {
-		//		cout << -1 << endl;
-		//		return 0;
-		//	}
-		//	cout << vec[1].size() << endl;
-		//	for (auto it = vec[1].rbegin(); it != vec[1].rend(); ++it) {
-		//		auto p = *it;
-		//		cout << p.size() << ' ';
-		//		for (auto &e : p) {
-		//			cout << e.u.id + 1 << ' ';
-		//		}
-		//		cout << p.rbegin()->v.id + 1 << '\n';
-		//	}
-
-		//int conn_id = 1;
-		//for (auto &two_conn : vec) {
-		//	fprintf(stderr, "component %d\n", conn_id);
-		//	int ear_id = 1;
-		//	for (auto &ear : two_conn) {
-		//		fprintf(stderr, "ear %d\n", ear_id);
-		//		for (auto &e : ear) {
-		//			fprintf(stderr, "\t%d -> %d\n", e.u.id, e.v.id);
-		//		}
-		//		++ear_id;
-		//	}
-		//	fprintf(stderr, "\n");
-		//	++conn_id;
-		//}
 
 		#else // __USE_DFS_TREE__ is not defined.
 		auto start = chrono::steady_clock::now();
